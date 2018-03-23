38b5d173fd06c362be2928f9724d488e48b8163b
johnnynode <johnnynode@gmail.com>
2018-03-23 20:53:02 +0800

    add object new attr methods

---
+++ es6/code/9.js
@@ -0,0 +1,17 @@
+var obj1 = {};
+var obj2 = {};
+
+module.exports = {obj1, obj2};
+
+/*
+module.exports.obj1 = obj1;
+module.exports.obj2 = obj2;
+
+甚至:
+
+module.exports = {
+  obj1:obj1,
+  obj2:obj2
+}
+
+*/
